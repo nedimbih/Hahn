ab98fb3ea496419ed071b0efd5c70a682b644531
NedimMulaosmanovic <nedimbih@hotmail.com>
2021-02-02 21:26:01 +0100

    Add project files.

---
+++ Hahn.ApplicationProcess.December2020.Models/Applicant.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hahn.ApplicationProcess.December2020.Models {
+	public class Applicant {
+		public int ID { get; set; }
+		public string Name { get; set; }
+		public string FamilyName { get; set; }
+		public string CountryOfOrigin { get; set; }
+		public string EMailAdress { get; set; }
+		public int Age { get; set; }
+		public bool Hired { get; set; }
+	}
+}
